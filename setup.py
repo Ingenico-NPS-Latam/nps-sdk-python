68c609b742379240e01bbce8ad1dcfdb23f4bb23
Inge <diazchg@gmail.com>
2016-12-12 16:46:48 -0300

    minorfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'nps',
   packages = ['nps'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.',
   description = 'A Python SDK for Ingenico ePayments - NPS LatAm Services',
   author = 'Gustavo Diaz',
   author_email = 'gustavo.diaz@ingenico.com',
@@ -10,5 +10,5 @@ setup(
   download_url = 'https://github.com/Ingenico-NPS-Latam/nps-sdk-python/tarball/0.1', # I'll explain this in a second
   keywords = ['ingenico', 'payments', 'nps', 'nps-sdk'], # arbitrary keywords
   classifiers = [],
-  install_requires=["suds-jurko= 0.6"]
+  install_requires=["suds-jurko=0.6"]
 )
